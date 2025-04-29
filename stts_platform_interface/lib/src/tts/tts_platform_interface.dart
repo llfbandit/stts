13a9f5aacf3a6023f78cb9ad6f4414758cb6a8e4
LLFBandit <llfbandit@gmail.com>
2025-04-29 18:43:40 +0200

    chore: Update documentation.

--- stts_platform_interface/lib/src/tts/tts_platform_interface.dart
+++ stts_platform_interface/lib/src/tts/tts_platform_interface.dart
@@ -17,17 +17,25 @@ abstract class TtsMethodChannelPlatformInterface {
   Future<bool> isSupported() async => false;
 
   /// Enqueues and starts an utterance from the given [text].
+  ///
+  /// Refer to [onStateChanged] for accurate state.
   Future<void> start(String text);
 
   /// Stops and clears all utterances.
+  ///
+  /// Refer to [onStateChanged] for accurate state.
   Future<void> stop();
 
   /// Pauses current utterance.
+  ///
+  /// Refer to [onStateChanged] for accurate state.
   Future<void> pause();
 
   /// Resumes current utterance from the closest "word".
   ///
   /// Android: Below API 26, current utterance will restart.
+  ///
+  /// Refer to [onStateChanged] for accurate state.
   Future<void> resume();
 
   /// Sets language for next utterance.
